08039d7ace8555c6860decd634ac6200b3671a4f
George <georgesims21@hotmail.com>
2019-12-27 00:58:56 +0100

    Body array created

--- demo.c
+++ demo.c
@@ -1,6 +1,7 @@
 /*
  *TODO:
  *    * Print a longer snake depending on direction and size of the snake
+ *      - Each node must follow the next node, like a linked list. Copies node infront's direction and next x and y.
  */
 
 #include <stdlib.h>
@@ -9,28 +10,28 @@
 #include <time.h>
 
 #define DELAY 60000
-#define HALF_SCREEN_SIZE 100
+#define MAX_SNAKE 100
 #define RIGHT 1
 #define LEFT -1
 #define UP 0
 #define DOWN 2
 
-typedef struct Object {
+typedef struct Snake {
     const char *shape;
     int x, y,
         next_x,
         next_y,
         y_direction,
         x_direction;
-} Object;
+} Snake;
 
 typedef struct Food {
     const char *shape;
     int x, y;
 } Food;
 
-void auto_move(int *last_direction, Object *circle);
-void change_direction(int ch, int *last_direction, Object *circle);
+void auto_move(int *last_direction, Snake *head);
+void change_direction(int ch, int *last_direction, Snake *head);
 WINDOW *init_screen(void);
 void random_food_generator(Food *food, int max_x, int max_y);
 
@@ -38,9 +39,10 @@ int main (int argc, char *argv[]){
 
     int ch = 0, last_direction = RIGHT, max_y = 0, max_x = 0, food_taken = 0;
     time_t t;
+    Snake body[MAX_SNAKE];
     getmaxyx(stdscr, max_y, max_x);
 
-    Object circle = {.shape = "O", .x = 0, .y = 0, .next_x = 0, 
+    Snake head = {.shape = "O", .x = 0, .y = 0, .next_x = 0, 
                     .next_y = 0, .y_direction = RIGHT, .x_direction = RIGHT};
     Food food = {.shape = "o", .x = 0, .y = 0};
 
@@ -55,9 +57,9 @@ int main (int argc, char *argv[]){
             random_food_generator(&food, max_x, max_y);
             food_taken = 0;
         }
-        mvprintw(circle.y, circle.x, circle.shape);
+        mvprintw(head.y, head.x, head.shape);
         mvprintw(food.y, food.x, food.shape);
-        if(circle.y == food.y && circle.x == food.x) {
+        if(head.y == food.y && head.x == food.x) {
             food_taken = 1;
         }
         refresh();
@@ -65,7 +67,7 @@ int main (int argc, char *argv[]){
 
         // Handle movements
         ch = getch();
-        change_direction(ch, &last_direction, &circle);
+        change_direction(ch, &last_direction, &head);
     }
 
 
@@ -76,28 +78,28 @@ int main (int argc, char *argv[]){
         // Clear current item(s) on screen
       //  clear();
        // if(ch = getch() == KEY_UP) {
-        //    mvprintw(circle.y, circle.x, circle.shape);
+        //    mvprintw(head.y, head.x, head.shape);
        // }
         
         //refresh();
 
-        //mvprintw(cap_circle.y, cap_circle.x, cap_circle.shape);
+        //mvprintw(cap_head.y, cap_head.x, cap_head.shape);
         //refresh();
 
         //usleep(DELAY);
-        //circle.next_x = circle.x + circle.x_direction;
-        //cap_circle.next_x = cap_circle.x + cap_circle.x_direction;
+        //head.next_x = head.x + head.x_direction;
+        //cap_head.next_x = cap_head.x + cap_head.x_direction;
 
-        //if(circle.next_x >= max_x || circle.next_x < 0) {
-        //    circle.x_direction *= -1;
+        //if(head.next_x >= max_x || head.next_x < 0) {
+        //    head.x_direction *= -1;
         //} else {
-            //circle.x += circle.x_direction;
+            //head.x += head.x_direction;
         //}
 
-        //if(cap_circle.next_x >= max_x || cap_circle.next_x < 0) {
-            //cap_circle.x_direction *= -1;
+        //if(cap_head.next_x >= max_x || cap_head.next_x < 0) {
+            //cap_head.x_direction *= -1;
         //} else {
-            //cap_circle.x += cap_circle.x_direction;
+            //cap_head.x += cap_head.x_direction;
         //}
    // }
 
@@ -112,39 +114,39 @@ void random_food_generator(Food *food, int max_x, int max_y) {
         food->y = (rand() % max_y);
 }
 
-void auto_move(int *last_direction, Object *circle) {
+void auto_move(int *last_direction, Snake *head) {
 
     if(*last_direction == UP) {
-        circle->y -= circle->y_direction;
+        head->y -= head->y_direction;
     } else if (*last_direction == RIGHT) {
         // Double to match speed of y axis
-        circle->x += 2*(circle->x_direction);
+        head->x += 2*(head->x_direction);
     } else if (*last_direction == DOWN) {
-        circle->y += circle->y_direction;
+        head->y += head->y_direction;
     } else if (*last_direction == LEFT) {
-        circle->x -= 2*(circle->x_direction);
+        head->x -= 2*(head->x_direction);
     }
 }
 
-void change_direction(int ch, int *last_direction, Object *circle) {
+void change_direction(int ch, int *last_direction, Snake *head) {
 
     if(ch == KEY_UP) {
-        circle->y -= circle->y_direction;
+        head->y -= head->y_direction;
         *last_direction = UP;
     } else if (ch == KEY_RIGHT) {
-        circle->x += circle->x_direction;
+        head->x += head->x_direction;
         *last_direction = RIGHT;
     } else if (ch == KEY_DOWN) {
-        circle->y += circle->y_direction;
+        head->y += head->y_direction;
         *last_direction = DOWN;
     } else if(ch == KEY_LEFT) {
-        circle->x -= circle->x_direction;
+        head->x -= head->x_direction;
         *last_direction = LEFT;
     } else if (ch == KEY_BACKSPACE){
         exit(0);
     } else {
         // No key press
-        auto_move(last_direction, circle);
+        auto_move(last_direction, head);
     }
 }
 
