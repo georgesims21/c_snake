c504ba3f077ea71bb582fdfd0fe1f980a534145a
George <georgesims21@hotmail.com>
2020-01-03 12:08:42 +0100

    Attempting to use a max_x and max_y ratio instead of ints, border not printing correctly upon game loop

--- demo.c
+++ demo.c
@@ -4,7 +4,6 @@
  * If going left can't go right, if going right can't go left... etc
  * Make all ints a percentage of the max screen size instead
  * Why doesn't the head register on certain food/borders?
- * Make main loop into method, then make menu choosing difficulty at start then run loop with DELAY as parameter
  */
 
 #include <stdlib.h>
@@ -38,33 +37,37 @@ void auto_move(int *last_direction, int total, Snake *snake);
 void change_direction(int ch, int *last_direction, int total, Snake *snake);
 void move_body(int total, Snake *snake);
 WINDOW *init_screen(void);
-void random_food_generator(Food *food, int max_x, int max_y);
+void random_food_generator(Food *food);
 void init_head(Snake *snake);
 void init_body(Snake *snake);
 void print_border(void);
-void game_loop(int delay, int max_x, int max_y);
+void game_loop(int delay);
 void menu_screen(void);
+int game_over(void);
+
+int max_x, max_y;
 
 int main (int argc, char *argv[]){
 
-    int max_x = 0, max_y = 0, ch = 0;
-    getmaxyx(stdscr, max_y, max_x);
+    int ch = 0, max_x_tmp = 0, max_y_tmp = 0;
     WINDOW *win = init_screen();
-    getmaxyx(stdscr, max_y, max_x);
     clear();
 
     for(;;){
+        getmaxyx(stdscr, max_y_tmp, max_x_tmp);
+        // Good box:screen ratio
+        max_x = max_x_tmp * 0.7; max_y = max_y_tmp * 0.7;
         menu_screen();
         ch = getch();
         if(ch == KEY_UP) {
             // EASY
-            game_loop(120000, max_x, max_y);
+            game_loop(120000);
         } else if (ch == KEY_RIGHT) {
             // MED
-            game_loop(90000, max_x, max_y);
+            game_loop(90000);
         } else if (ch == KEY_DOWN) {
             // HARD
-            game_loop(30000, max_x, max_y);
+            game_loop(30000);
         } else if (ch == KEY_BACKSPACE) {
             // EXIT
             clear();
@@ -75,22 +78,6 @@ int main (int argc, char *argv[]){
 
     endwin();
     return 0;
-}
-
-void print_border(void) {
-
-        mvprintw(1, 1, "+");
-        mvprintw(49, 1, "+");
-        mvprintw(1, 99, "+");
-        mvprintw(49, 99, "+");
-        for(int i = 2; i <= 48; i++) {
-            mvprintw(i, 1, "|");
-            mvprintw(i, 99, "|");
-        }
-        for(int i = 2; i <= 98; i++) {
-            mvprintw(1, i, "-");
-            mvprintw(49, i, "-");
-        }
 }
 
 void init_body(Snake *snake) {
@@ -111,6 +98,7 @@ void init_body(Snake *snake) {
 void init_head(Snake *snake){
 
     snake[0].shape = "O";
+    // Avoid starting on border
     snake[0].x = 2;
     snake[0].y = 2;
     snake[0].next_x = 0;
@@ -121,10 +109,11 @@ void init_head(Snake *snake){
     snake[0].y_direction = RIGHT;
 }
 
-void random_food_generator(Food *food, int max_x, int max_y) {
+void random_food_generator(Food *food) {
 
-        food->x = ((rand() % 95) + 2);
-        food->y = ((rand() % 42) + 2);
+    // Avoid printing food on borders
+    food->x = ((rand() % (max_x - 3)) + 2);
+    food->y = ((rand() % (max_y - 3)) + 2);
 }
 
 void move_body(int total, Snake *snake) {
@@ -165,25 +154,21 @@ void change_direction(int ch, int *last_direction, int total, Snake *snake) {
             snake[0].y -= snake[0].y_direction;
             *last_direction = UP;
         }
-        auto_move(last_direction, total, snake);
     } else if (ch == KEY_RIGHT) {
         if(*last_direction != LEFT) {
             snake[0].x += snake[0].x_direction;
             *last_direction = RIGHT;
         }
-        auto_move(last_direction, total, snake);
     } else if (ch == KEY_DOWN) {
         if(*last_direction != UP) {
             snake[0].y += snake[0].y_direction;
             *last_direction = DOWN;
         }
-        auto_move(last_direction, total, snake);
     } else if(ch == KEY_LEFT) {
         if(*last_direction != RIGHT) {
             snake[0].x -= snake[0].x_direction;
             *last_direction = LEFT;
         }
-        auto_move(last_direction, total, snake);
     } else if (ch == KEY_BACKSPACE){
         endwin();
         exit(0);
@@ -193,7 +178,7 @@ void change_direction(int ch, int *last_direction, int total, Snake *snake) {
     }
 }
 
-void game_loop(int delay, int max_x, int max_y) {
+void game_loop(int delay) {
 
     int ch = 0, last_direction = RIGHT, 
             food_taken = 1, total = 0;
@@ -210,32 +195,36 @@ void game_loop(int delay, int max_x, int max_y) {
         clear();
         print_border();
         if(food_taken) {
-            random_food_generator(&food, max_x, max_y);
+            random_food_generator(&food);
             food_taken = 0;
         }
         // Print snake
         for(int i = 0; i <= total; i++) {
+            if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+                /*if(game_over() == 0) {*/
+                    /*return;*/
+                /*} else {*/
+                    /*exit(0);*/
+                /*}*/
+            }
             mvprintw(snake[i].y, snake[i].x, snake[i].shape);
         }
         mvprintw(food.y, food.x, food.shape);
-        mvprintw(0, 46, "Total: %d", total);
+        /*mvprintw(0, (max_x / 4), "Total: %d", total);*/
+        mvprintw(0, (max_x / 4), "max_x: %d\tmax_y: %d", max_x, max_y);
 
         if(snake[0].y == food.y && snake[0].x == food.x) {
             food_taken = 1;
             total++;
-        } else if (snake[0].y == 1 || snake[0].y == 49  || \
-                    snake[0].x == 1 || snake[0].x == 98) {
-
-            clear();
-            print_border();
-            mvprintw(25, 40, "GAME OVER");
-            mvprintw(26, 40, "Press backspace to restart");
-            for(;;) {
-                ch = getch();
-                if(ch == KEY_BACKSPACE) {
-                    return;
-                }
+        } else if (snake[0].y == 1 || snake[0].y == max_y  || \
+                    snake[0].x == 1 || snake[0].x == max_x) {
+            
+            if(game_over() == 0) {
+                return;
+            } else {
+                exit(0);
             }
+
         }
         
         refresh();
@@ -246,14 +235,45 @@ void game_loop(int delay, int max_x, int max_y) {
     }
 }
 
+int game_over(void) {
+
+    int ch;
+    clear();
+    print_border();
+    mvprintw((max_y * 0.35), (max_x / 2), "GAME OVER");
+    mvprintw((max_y * 0.35) + 1, (max_x/2), "Press backspace to restart");
+    for(;;) {
+        ch = getch();
+        if(ch == KEY_BACKSPACE) {
+            return 0;
+        }
+    }
+}
+
+void print_border(void) {
+
+        mvprintw(1, 1, "+");
+        mvprintw(max_y, 1, "+");
+        mvprintw(1, max_x, "+");
+        mvprintw(max_y, max_x, "+");
+        for(int i = 2; i <= (max_y - 1); i++) {
+            mvprintw(i, 1, "|");
+            mvprintw(i, max_x, "|");
+        }
+        for(int i = 2; i <= (max_x - 1); i++) {
+            mvprintw(1, i, "-");
+            mvprintw(max_y, i, "-");
+        }
+}
+
 void menu_screen(void) {
 
     print_border();
-    mvprintw(25,35, "Welcome to snake!");
-    mvprintw(26,35, "Press the up arrow for EASY");
-    mvprintw(27,35, "Press the right arrow for MEDIUM");
-    mvprintw(28,35, "Press the down arrow for HARD");
-    mvprintw(29,35, "Press backspace to exit");
+    mvprintw((max_y * 0.35), (max_x * 0.25), "Welcome to snake!");
+    mvprintw((max_y * 0.35) + 1, (max_x * 0.25), "Press the up arrow for EASY");
+    mvprintw((max_y * 0.35) + 2, (max_x * 0.25), "Press the right arrow for MEDIUM");
+    mvprintw((max_y * 0.35) + 3, (max_x * 0.25), "Press the down arrow for HARD");
+    mvprintw((max_y * 0.35) + 4, (max_x * 0.25), "Press backspace to exit");
 }
 
 WINDOW *init_screen(void) {
